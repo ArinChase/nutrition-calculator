d4badfecea027f74981ba2c0d4cc59d5e1f90308
Arin Chase <47833308+ArinChase@users.noreply.github.com>
2021-03-27 18:01:14 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import App from 'next/app'
+import Layout from '../components/layout'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../public/css/main.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default class MyApp extends App {
+  render () {
+    const { Component, pageProps } = this.props
+    return (
+      <Layout>
+      	<Component {...pageProps} />
+      </Layout>
+    )
+  }
 }
-
-export default MyApp

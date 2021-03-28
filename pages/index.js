823f8c4046134fbe799821e456bbd2b1cdba5d50
Arin Chase <47833308+ArinChase@users.noreply.github.com>
2021-03-27 20:32:00 -0500

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ const NutritionHome = () => {
   function renderData() {
     if(data) {
       return (
-        <div className="card mb-3">
+        <div className="card ac-super-light ac-bg">
           <div className="card-body text-dark">
             <h5 className="card-title"><Image className="thumbnail pb-2 mx-2" src={data.foods[0].photo.thumb} /> {data.foods[0].food_name} </h5>
             <table class="table card-text">
@@ -48,17 +48,16 @@ const NutritionHome = () => {
               </thead>
               <tbody>
                 <tr>
-                  <td> {data.foods[0].nf_calories} </td>
-                  <td> {data.foods[0].nf_sodium} </td>
-                  <td> {data.foods[0].nf_total_fat} </td>
-                  <td> {data.foods[0].nf_saturated_fat} </td>
-                  <td> {data.foods[0].nf_cholesterol} </td>
-                  <td> {data.foods[0].nf_total_carbohydrate} </td>
-                  <td> {data.foods[0].nf_dietary_fiber} </td>
-                  <td> {data.foods[0].nf_sugars} </td>
-                  <td> {data.foods[0].nf_protein} </td>
-                  <td> {data.foods[0].nf_potassium} </td>
-                  
+                  <td> {data.foods[0].nf_calories}g </td>
+                  <td> {data.foods[0].nf_sodium}g </td>
+                  <td> {data.foods[0].nf_total_fat}g </td>
+                  <td> {data.foods[0].nf_saturated_fat}g </td>
+                  <td> {data.foods[0].nf_cholesterol}g </td>
+                  <td> {data.foods[0].nf_total_carbohydrate}g </td>
+                  <td> {data.foods[0].nf_dietary_fiber}g </td>
+                  <td> {data.foods[0].nf_sugars}g </td>
+                  <td> {data.foods[0].nf_protein}g </td>
+                  <td> {data.foods[0].nf_potassium}g </td>     
                 </tr>
               </tbody>
             </table>
@@ -80,21 +79,22 @@ const NutritionHome = () => {
         <div className="navSpace"></div>
         <Image src="/images/food.png" className="m-3 ac-super-light ac-bg p-3 nutrition-icon" roundedCircle />
         <h1 className="display-4 pt-4 ac-super-light ac-text"><strong>Nutrition Calculator</strong></h1>
-        <h4 className="px-2 py-4 ac-super-light ac-text">Find nutritional info on ingredients and meals</h4>
+        <h4 className="px-2 py-4 ac-super-light ac-text"> - Powered by <a className="ac-grey-medium ac-text font-weight-bold" href="https://nutritionix.com" target="_blank">Nutritionix Api</a></h4>
       </div>
       
       <div className="container ac-super-light ac-bg">
         <div className="text-center" id="search">
           <div className="navSpace"></div>
+          <h4 className="px-2 pt-2 pb-5 ac-grey-medium ac-text">Find nutritional info on ingredients and meals</h4>
+          
           <form onSubmit={fetchData} className="ac-cadet-blue">
             <div className="field">
               <input className="button" type="submit" value="Search" name="submit" />
               <input type="text" placeholder="Enter an ingredient here..." name="input" autoComplete="off" required /> 
             </div>
           </form>
-          <div className="navSpace"></div>
         </div>
-        {renderData()}
+        <div class="p-5">{renderData()}</div>
       </div>
     </main>
   </>
